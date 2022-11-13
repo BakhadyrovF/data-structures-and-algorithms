0258d2358fb5842ecedfcdda5c4df89cb0f0c4a8
BakhadyrovF <BakhadyrovF@gmail.com>
2022-11-13 19:39:14 +0500

    arrays: two sum

---
+++ src/data_structures/arrays/optional/two_sum.php
@@ -0,0 +1,29 @@
+<?php
+
+$array = [1, 3, 4, 6, 10];
+$sum = 16;
+
+/**
+ * Solution - O(n)
+ *
+ * Steps:
+ * 1. Initialize new empty array variable $cache
+ * 2. Iterate each element of array
+ * 3. Check if $cache has element in index ($sum - $array[$i]) then return indices
+ * 4. Else ($cache has no element in this index) then we add value of $i for index ($sum - $array[$i]) to $cache array
+ *
+ */
+function twoSum(array $array, int $sum)
+{
+    $cache = [];
+
+    for ($i = 0; $i < count($array); $i++) {
+        if (isset($cache[$array[$i]])) {
+            return [$cache[$array[$i]], $i];
+        }
+
+        $cache[$sum - $array[$i]] = $i;
+    }
+
+    return -1;
+}
