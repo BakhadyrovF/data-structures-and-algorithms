211cb5dfd80f80a25994d91d5234f368192b60df
Firuzbek <bakhadyrovf@gmail.com>
2023-01-12 13:17:47 +0500

    sorting: insertion sort - ref

--- src/algorithms/sorting/insertion_sort/approach_2/implementation.php
+++ src/algorithms/sorting/insertion_sort/approach_2/implementation.php
@@ -2,35 +2,11 @@
 
 $numbers = [2, 5, 4, 3, 2, 6, 3, 5, 1, 4, 2, 6];
 
-
-// Time complexity - O(n^2)
-// Space complexity - O(1)
-function myInsertionSort(array $array)
-{
-    for ($i = 1; $i < count($array); $i++) {
-        if ($array[$i] < $array[0]) {
-            // swap
-            array_unshift($array, array_splice($array, $i, 1)[0]);
-        } else {
-            for ($j = $i - 1; $j > 0; $j--) {
-                if ($array[$i] < $array[$j] && $array[$i] >= $array[$j - 1]) {
-                    // simple adding element at specific index and shift rest of the elements with built-in function
-                    array_splice($array, $j, 0, array_splice($array, $i, 1)[0]);
-                }
-            }
-        }
-    }
-
-    return $array;
-}
-
-
-print_r(myInsertionSort($numbers));
-
-
-// Usage of O(n) space complexity, makes algorithm faster and simpler without (shifts, splices)
-// Time complexity - O(n^2)
-// Space complexity - O(n)
+/**
+ * Usage of O(n) space complexity, makes algorithm faster and simpler without (shifts, splices)
+ * Time complexity - O(n^2)
+ * Space complexity - O(n)
+*/
 function mySimpleInsertionSort(array $array)
 {
     $sortedArray = [];
@@ -57,6 +33,3 @@ function mySimpleInsertionSort(array $array)
 
     return $sortedArray;
 }
-
-//print_r(mySimpleInsertionSort($numbers)); // [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
-
